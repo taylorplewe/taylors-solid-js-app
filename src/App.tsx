0dbc11001e18d776bb9731e4f7a243dcb9b6389b
Taylor Plewe <taylor.plewe@awardco.com>
2025-03-20 07:24:42 -0600

    use createSignal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-// import { createSignal } from 'solid-js'
-import './App.css'
+import { createSignal } from 'solid-js';
+import './App.css';
 
 export default () => {
-  const num: number = 34;
+  const [num, setNum] = createSignal(0);
 
   return (
     <>
-      <h1>ayoooooo {num}</h1>
+      <h1>Hello, Solid!</h1>
+      <p>{num()}</p>
+      <button onClick={() => setNum(num() + 1)}>Increment</button>
     </>
   )
 }
